ac39165626f9a2b34a3c8e39cb2184dbb4c7b346
ztgoto <ztmailgo@gmail.com>
2017-07-07 19:50:00 +0800

    test routing

--- http/http.go
+++ http/http.go
@@ -1,9 +1,11 @@
 package http
 
 import (
+	"bufio"
 	"github.com/valyala/fasthttp"
 	"github.com/ztgoto/webrouting/config"
 	"log"
+	"net"
 )
 
 func StartServer(cf *config.HttpConfig) {
@@ -25,4 +27,19 @@ func request(ctx *fasthttp.RequestCtx) {
 	log.Printf("request path:%q\n", ctx.Path())
 	log.Printf("Headers:\n%s\n", ctx.Request.Header.Header())
 	log.Printf("Body:%v\n\n\n", ctx.Request.Body())
+
+	serverConn, err := net.Dial("tcp", "192.168.4.132:8026")
+
+	if err != nil {
+		log.Printf("end server exception%v\n", err)
+		ctx.Response.SetStatusCode(500)
+		ctx.Response.SetBodyString("server error")
+		return
+	}
+	defer serverConn.Close()
+
+	ctx.Request.WriteTo(serverConn)
+
+	ctx.Response.Read(bufio.NewReader(serverConn))
+
 }
