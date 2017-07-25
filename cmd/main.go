6996b6589d556d4c9a17c2697bfb52ecc6574f87
ztgoto <ztmailgo@gmail.com>
2017-07-25 17:51:42 +0800

    add lib

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,34 @@
 package main
 
-import (
-	"github.com/ztgoto/webrouting/config"
-	server "github.com/ztgoto/webrouting/http"
-	"log"
-	"runtime"
-)
+import "fmt"
 
-func init() {
-	cpuNum := runtime.NumCPU()
-	log.Printf("CPU Num:%d\r\n", cpuNum)
+// "log"
+// "runtime"
 
-	runtime.GOMAXPROCS(config.SC.MaxProcs)
-	log.Printf("GOMAXPROCS:%d\r\n", config.SC.MaxProcs)
-}
+// "github.com/ztgoto/webrouting/config"
+// server "github.com/ztgoto/webrouting/http"
+
+const banner string = `
+
+                  ___.                                   __  .__                
+    __  _  __ ____\_ |__           _______  ____  __ ___/  |_|__| ____    ____  
+    \ \/ \/ // __ \| __ \   ______ \_  __ \/  _ \|  |  \   __\  |/    \  / ___\ 
+     \     /\  ___/| \_\ \ /_____/  |  | \(  <_> )  |  /|  | |  |   |  \/ /_/  >
+      \/\_/  \___  >___  /          |__|   \____/|____/ |__| |__|___|  /\___  / 
+                 \/    \/                                            \//_____/  
+
+`
+
+// func init() {
+// 	cpuNum := runtime.NumCPU()
+// 	log.Printf("CPU Num:%d\r\n", cpuNum)
+
+// 	runtime.GOMAXPROCS(config.SC.MaxProcs)
+// 	log.Printf("GOMAXPROCS:%d\r\n", config.SC.MaxProcs)
+// }
 
 func main() {
-	log.Println(config.ServerConfig)
-	server.StartServer(&config.ServerConfig)
+	// log.Println(config.ServerConfig)
+	// server.StartServer(&config.ServerConfig)
+	fmt.Println(banner)
 }
