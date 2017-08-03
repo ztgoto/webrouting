001385c3c2cf7aab6ccae523670ed35d9c3bef90
ztgoto <ztmailgo@gmail.com>
2017-08-03 18:44:02 +0800

    test

--- http/http.go
+++ http/http.go
@@ -2,36 +2,39 @@ package http
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/valyala/fasthttp"
 	"github.com/ztgoto/webrouting/config"
 )
 
+var (
+	w sync.WaitGroup
+)
+
 // StartServer 启动http服务
 func StartServer() {
-
-	// Start HTTP server.
-	// if len((*cf).Addr) > 0 {
-
-	// 	log.Printf("Starting HTTP server on %q", (*cf).Addr)
-	// 	if err := fasthttp.ListenAndServe((*cf).Addr, RootHandler); err != nil {
-	// 		log.Fatalf("error in ListenAndServe: %s", err)
-	// 	}
-
-	// }
-
+	w.Add(1)
+	for k, v := range config.ListenServerList {
+		fmt.Printf("Listing addr:%s\n", k)
+		go func(sf *config.ServerData) {
+			w.Add(1)
+			defer w.Done()
+			e := AddServer(sf)
+			if e != nil {
+				fmt.Println(e)
+			}
+		}(v)
+	}
+	w.Wait()
 }
 
 // AddServer 添加启动http服务
-func AddServer(sf *config.ServerData) {
+func AddServer(sf *config.ServerData) (e error) {
 	if sf.SSL {
-		if err := fasthttp.ListenAndServeTLS(sf.Listen, sf.CertFile, sf.KeyFile, RootHandler); err != nil {
-			fmt.Println(err)
-		}
+		e = fasthttp.ListenAndServeTLS(sf.Listen, sf.CertFile, sf.KeyFile, RootHandler)
 	} else {
-
-		if err := fasthttp.ListenAndServe(sf.Listen, RootHandler); err != nil {
-			fmt.Println(err)
-		}
+		e = fasthttp.ListenAndServe(sf.Listen, RootHandler)
 	}
+	return
 }
