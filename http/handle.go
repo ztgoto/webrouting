9ecd18085c10283a5bb7e5187135fd865b1a349d
ztgoto <ztmailgo@gmail.com>
2017-08-04 19:03:46 +0800

    test

--- http/handle.go
+++ http/handle.go
@@ -1,11 +1,8 @@
 package http
 
 import (
-	"bufio"
 	"context"
 	"fmt"
-	"log"
-	"net"
 
 	"github.com/valyala/fasthttp"
 	"github.com/ztgoto/webrouting/config"
@@ -44,24 +41,25 @@ func (s *ServerContext) RequestHandler(ctx *fasthttp.RequestCtx) {
 // DefaultHandle 基础http请求处理器
 func DefaultHandle(c context.Context, ctx *fasthttp.RequestCtx) error {
 
-	log.Printf("request path:%q\n", ctx.Path())
-	log.Printf("Request Headers:\n%s\n", ctx.Request.Header.Header())
-	log.Printf("Request Body:%v\n\n\n", ctx.Request.Body())
+	// log.Printf("request path:%q\n", ctx.Path())
+	// log.Printf("Request Headers:\n%s\n", ctx.Request.Header.Header())
+	// log.Printf("Request Body:%v\n\n\n", ctx.Request.Body())
+	// log.Printf("Request Host:-%s-\n\n\n", ctx.Request.Host())
 
-	serverConn, err := net.Dial("tcp", "127.0.0.1:80")
+	// serverConn, err := net.Dial("tcp", "127.0.0.1:80")
 
-	if err != nil {
-		log.Printf("end server exception%v\n", err)
-		ctx.Response.SetStatusCode(config.HTTPStatusBadGateway)
-		ctx.Response.SetBodyString("Bad Gateway")
-		return nil
-	}
-	defer serverConn.Close()
+	// if err != nil {
+	// 	log.Printf("end server exception%v\n", err)
+	// 	ctx.Response.SetStatusCode(config.HTTPStatusBadGateway)
+	// 	ctx.Response.SetBodyString("Bad Gateway")
+	// 	return err
+	// }
+	// defer serverConn.Close()
 
-	ctx.Request.WriteTo(serverConn)
+	// ctx.Request.WriteTo(serverConn)
 
-	ctx.Response.Read(bufio.NewReader(serverConn))
-	log.Printf("Response Headers:\n%s\n", ctx.Response.Header.Header())
-	ctx.Response.Header.Set("Server", "fasthttp")
+	// ctx.Response.Read(bufio.NewReader(serverConn))
+	// log.Printf("Response Headers:\n%s\n", ctx.Response.Header.Header())
+	// ctx.Response.Header.Set("Server", "fasthttp")
 	return nil
 }
